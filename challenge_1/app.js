412252fddf218e08bfcbf1404501317891565ae9
Kang Jie Tan <kangjietan@gmail.com>
2020-01-13 11:01:09 -0800

    Add event listener for each box

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,22 @@
+var rowCol = document.getElementsByTagName('td');
+console.log(rowCol);
+
+var check = true;
 
 
+const markBoard = (item) => { 
+  console.log(item);
+  var mark = check ? 'X' : 'O';
+  item.innerHTML = mark; 
+  check = !check;
+  console.log(check);
+}
+
+for (var i = 0; i < rowCol.length; i++) {
+  console.log(rowCol[i]);
+  var test = rowCol[i];
+  rowCol[i].addEventListener("click", (e) => { 
+    console.log(e.target);
+    markBoard(e.target); 
+  });
+}
