9d3f886b1ad6f7d85cd657d9875451e3314a3f3b
Kang Jie Tan <kangjietan@gmail.com>
2020-01-14 10:46:32 -0800

    Serve static files

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,7 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 
+// console.log(path.join(__dirname, 'client'));
 
+app.use(express.static(path.join(__dirname, 'client')));
