dcc1f93bfd9b54359da01a1eb88e7b4d92591660
Kang Jie Tan <kangjietan@gmail.com>
2020-01-14 14:15:04 -0800

    Send file for client to download

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
-// console.log(path.join(__dirname, 'client'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(3000);
 
 app.use(express.static(path.join(__dirname, 'client')));
+
+app.post('/submit', (req, res, next) => {
+  console.log('Client sent POST request!', req.body);
+  // res.download('./samples/csv_report.csv', 'csv_report.csv', (err) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log('Success');
+  //   }
+  // });
+
+  // var options = {
+  //   root: path.join(__dirname, 'samples')
+  // }
+
+  // res.sendFile('csv_report.csv', options, (err) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log('success');
+  //   }
+  // })
+});
