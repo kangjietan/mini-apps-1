3f01d5c86a67c543b5461634047d5de5da4d01ee
Kang Jie Tan <kangjietan@gmail.com>
2020-01-14 15:54:58 -0800

    Flatten data, filter it, and stringify it

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -11,7 +11,54 @@ app.listen(3000);
 app.use(express.static(path.join(__dirname, 'client')));
 
 app.post('/submit', (req, res, next) => {
-  console.log('Client sent POST request!', req.body);
+  // console.log('Client sent POST request!', req.body.input);
+  var clientInput = JSON.parse(req.body.input)
+  // console.log(clientInput);
+  var results = [];
+  var strings = [];
+  
+  // Make JSON data more easily accessible
+  var flattenData = (obj) => {
+    var result = [];
+    var inputKeys = Object.keys(obj);
+    for (var i = 0; i < inputKeys.length; i++) {
+      var k = inputKeys[i];
+      if (Array.isArray(obj[k])) {
+        for (var j = 0; j < obj[k].length; j++) {
+          results.push((flattenData(obj[k][j])));
+        }
+      }
+      
+      if (!Array.isArray(obj[k])) {
+        result.push([k, obj[k]]);
+      }
+    }
+
+    return result;
+  }
+
+  results.push(flattenData(clientInput));
+  // console.log(results);
+
+  // Convert into string
+  for (var i = 0; i < results.length; i++) {
+    var innerArray = results[i];
+    // console.log(i, innerArray.length);
+    var row = '';
+    for (var j = 0; j < innerArray.length; j++) {
+      row += innerArray[j];
+      if (j === innerArray.length - 1) {
+        row += '/n';
+      }
+    }
+
+    strings.push(row);
+  }
+
+  console.log(strings);
+
+  
+  res.end();
   // res.download('./samples/csv_report.csv', 'csv_report.csv', (err) => {
   //   if (err) {
   //     console.log(err);
