3e471d94d39f68665fe1fa3cb1fa6cc362b969b6
Kang Jie Tan <kangjietan@gmail.com>
2020-01-15 11:41:02 -0800

    Set up Form1, 2, and 3 components

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,13 +1,54 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      page: 0
+    }
+  }
+
+  handleButtonClick() {
+    this.setState({page: page + 1});
   }
 
   render () {
     return (
-      <div>Hello</div>
+      <div>
+        <button onClick={this.handleButtonClick}>Checkout</button>
+        <F1 />
+      </div>
     );
   }
 }
+
+// name, email, password for account creation
+var F1 = () => (
+  <div>
+    <form>
+      <input type="text"></input>
+    </form>
+    <button>Next</button>
+  </div>
+);
+
+// ship to address (line 1, line 2, state, zip code) and phone number
+var F2 = () => (
+  <div>
+    <form>
+      <input type="text"></input>
+    </form>
+    <button>Next</button>
+  </div>
+);
+
+// credit card number, expiracy date, CVV, and billing zip code
+var F3 = () => (
+  <div>
+    <form>
+      <input type="text"></input>
+    </form>
+    <button>Next</button>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
