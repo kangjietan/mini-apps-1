142b4b5faef1641ecf59ad9d8d9c594eb71f472b
Kang Jie Tan <kangjietan@gmail.com>
2020-01-14 17:28:38 -0800

    Write to file after formatting

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+var Promise = require('bluebird');
+const writeFileAsync = Promise.promisify(fs.writeFile);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,6 +19,7 @@ app.post('/submit', (req, res, next) => {
   // console.log(clientInput);
   var results = [];
   var strings = [];
+  var columns = [];
   
   // Make JSON data more easily accessible
   var flattenData = (obj) => {
@@ -30,8 +33,12 @@ app.post('/submit', (req, res, next) => {
         }
       }
       
+      // Filter for non-array
       if (!Array.isArray(obj[k])) {
-        result.push([k, obj[k]]);
+        if (columns.indexOf(k) < 0) {
+          columns.push(k);
+        } 
+        result.push([obj[k]]);
       }
     }
 
@@ -47,7 +54,7 @@ app.post('/submit', (req, res, next) => {
     // console.log(i, innerArray.length);
     var row = '';
     for (var j = 0; j < innerArray.length; j++) {
-      row += innerArray[j];
+      row += innerArray[j] + ', ';
       if (j === innerArray.length - 1) {
         row += '\n';
       }
@@ -56,20 +63,50 @@ app.post('/submit', (req, res, next) => {
     strings.push(row);
   }
 
+  // Add new line to column names
+  columns.push('\n');
+  // console.log(strings);
+
+  // Flatten arr to turn into one string with newline
   const flattened = arr => { return [].concat(...arr) };
   var flatString = flattened(strings);
-  // console.log(...strings);
-  // for (var i = 0; i < strings.length; i++) {
-  fs.writeFileSync('test.txt', flatString, (err) => {
+  // flatString = flatString.join('');
+  
+  // Add column names as well
+  // columns = columns.join(' ');
+  flatString = columns.concat(flatString);
+  // console.log(flatString);
+
+  // Write to file that one string
+  writeFileAsync('test.txt', flatString, (err) => {
     if (err) {
       console.log(err);
     } else {
       console.log('Writing!');
     }
   })
-  // }
+  .then(
+    () => {
+      // res.download('./test.txt', 'test.txt', (err) => {
+      //   if (err) {
+      //     console.log(err);
+      //   } else {
+      //     console.log('Success');
+      //   }
+      // });
 
-  // res.end();
+      var options = {
+        root: path.join(__dirname)
+      } 
+
+    res.sendFile('test.txt', options, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('success');
+      }
+    })
+  });
   // res.download('./samples/csv_report.csv', 'csv_report.csv', (err) => {
   //   if (err) {
   //     console.log(err);
