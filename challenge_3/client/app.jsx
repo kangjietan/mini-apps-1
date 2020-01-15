f6c1c3cd23f98e6b02f9250c2d390bf43c9c211c
Kang Jie Tan <kangjietan@gmail.com>
2020-01-15 11:16:17 -0800

    Set up App component

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,13 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render () {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
 
+ReactDOM.render(<App />, document.getElementById('app'));
