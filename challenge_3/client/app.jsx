c614948931a8a1a8312babefdb4cb8455a324ade
Kang Jie Tan <kangjietan@gmail.com>
2020-01-15 12:32:26 -0800

    Set up inputs for form1 and form2

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -3,7 +3,10 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      page: 0
+      page: 0,
+      f1information: '',
+      f2information: '',
+      f3information: ''
     };
 
     this.handleButtonClick = this.handleButtonClick.bind(this);
@@ -14,8 +17,9 @@ class App extends React.Component {
   }
 
   render () {
+    // Cycles through pages 0-4 only
+    // 0: checkout 1: form 1, 2: form 2, 3: form 3, 4: purchase
     var currentPage = this.state.page % 5;
-    console.log(currentPage);
 
     if (currentPage === 0) {
       return (
@@ -58,12 +62,25 @@ class App extends React.Component {
   }
 }
 
+////////////////////// FORMS and purchase page /////////////////////////
+
 // name, email, password for account creation
 var F1 = (props) => (
   <div>
-    <h1>Form 1</h1>
+    <h1>Form 1: 1st step</h1>
     <form>
-      <input type="text"></input>
+      <div>
+        <label>Name: </label>
+        <input type="text" name="name"></input>
+      </div>
+      <div>
+        <label>Email: </label>
+        <input type="email" name="email"></input>
+      </div>
+      <div>
+        <label>Password: </label>
+        <input type="password" name="password"></input>
+      </div>
     </form>
     <button onClick={props.pageChange}>Next</button>
   </div>
@@ -72,9 +89,29 @@ var F1 = (props) => (
 // ship to address (line 1, line 2, state, zip code) and phone number
 var F2 = (props) => (
   <div>
-    <h1>Form 2</h1>
+    <h1>Form 2: 2nd step</h1>
     <form>
-      <input type="text"></input>
+      <h2>Address</h2>
+      <div>
+        <label>Line 1: </label>
+        <input type="text" name="line1"></input>
+      </div>
+      <div>
+        <label>Line 2: </label>
+        <input type="text" name="line2"></input>
+      </div>
+      <div>
+        <label>State: </label>
+        <input type="text" name="state"></input>
+      </div>
+      <div>
+        <label>Zip Code: </label>
+        <input type="text" name="zip"></input>
+      </div>
+      <div>
+        <label>Phone Number: </label>
+        <input type="tel" name="phone"></input>
+      </div>
     </form>
     <button onClick={props.pageChange}>Next</button>
   </div>
@@ -83,7 +120,7 @@ var F2 = (props) => (
 // credit card number, expiracy date, CVV, and billing zip code
 var F3 = (props) => (
   <div>
-    <h1>Form 3</h1>
+    <h1>Form 3: 3rd step</h1>
     <form>
       <input type="text"></input>
     </form>
@@ -91,9 +128,10 @@ var F3 = (props) => (
   </div>
 );
 
+// Last page summarizing all the data
 var Purchase = (props) => (
   <div>
-    <h1>Purchase</h1>
+    <h1>Purchase: Final step</h1>
     <button onClick={props.pageChange}>Purchase</button>
   </div>
 );
